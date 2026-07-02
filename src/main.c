a75e612d279b7aae01303e5e348ecf5aef171c08
David Clonts <david.clonts@eagles.oc.edu>
2026-07-02 14:47:52 -0500

    todos and bot file

--- src/main.c
+++ src/main.c
@@ -4,7 +4,9 @@
 
 // Headers
 
-
+// TODO: make file
+// TODO: func that takes the struct and gives its name such as: "Queen of hearts"
+// TODO: test by logging all cards by name using func
 
 int main(void) {
     printf("This is the start of some serious go fishin \n");
