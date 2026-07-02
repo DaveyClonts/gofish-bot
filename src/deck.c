1667400179a16f5c966f0c6b8d76ae983b1dbbd2
David Clonts <david.clonts@eagles.oc.edu>
2026-07-02 14:44:36 -0500

    deck is made

---
+++ src/deck.c
@@ -0,0 +1,53 @@
+
+enum suits {
+    CLUB,
+    DIAMOND,
+    HEART,
+    SPADES
+};
+
+enum types {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+};
+
+struct card {
+    enum suits suit;
+    enum types type;
+};
+
+struct card deck[52] = {
+    // CLUB
+    {CLUB, TWO}, {CLUB, THREE}, {CLUB, FOUR}, {CLUB, FIVE}, {CLUB, SIX},
+    {CLUB, SEVEN}, {CLUB, EIGHT}, {CLUB, NINE}, {CLUB, TEN}, {CLUB, JACK},
+    {CLUB, QUEEN}, {CLUB, KING}, {CLUB, ACE},
+
+    // DIAMOND
+    {DIAMOND, TWO}, {DIAMOND, THREE}, {DIAMOND, FOUR}, {DIAMOND, FIVE}, {DIAMOND, SIX},
+    {DIAMOND, SEVEN}, {DIAMOND, EIGHT}, {DIAMOND, NINE}, {DIAMOND, TEN}, {DIAMOND, JACK},
+    {DIAMOND, QUEEN}, {DIAMOND, KING}, {DIAMOND, ACE},
+
+    // HEART
+    {HEART, TWO}, {HEART, THREE}, {HEART, FOUR}, {HEART, FIVE}, {HEART, SIX},
+    {HEART, SEVEN}, {HEART, EIGHT}, {HEART, NINE}, {HEART, TEN}, {HEART, JACK},
+    {HEART, QUEEN}, {HEART, KING}, {HEART, ACE},
+
+    // SPADES
+    {SPADES, TWO}, {SPADES, THREE}, {SPADES, FOUR}, {SPADES, FIVE}, {SPADES, SIX},
+    {SPADES, SEVEN}, {SPADES, EIGHT}, {SPADES, NINE}, {SPADES, TEN}, {SPADES, JACK},
+    {SPADES, QUEEN}, {SPADES, KING}, {SPADES, ACE}
+};
+
+//better way to do this might be to make an ID that is H2-HA or what not... idk ill come back to this
+
